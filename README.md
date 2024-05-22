06dde09a21cd91a4971b9236a620012714d9537c
Entropt <entropt05@gmail.com>
2024-05-22 17:14:11 +0700

    finish posts panel

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-## Vunerable Website by Entropt
+# Vunerable Website by Entropt
+
+## Requirement
+SQLi (Labs: done) <br>
+Insecure Deserialization <br>
+File upload (Labs: done) <br>
+XSS <br>
+IDOR (Labs: done) <br>
+Authentication <br>
+
+## Bonus:
+Server-side template injection
+Race condition (with File Upload)
+
+## Website Analysis:
+A website with normal login <br>
+With picture profile upload <br>
+A blog website <br>
+IDOR with the pointer to secret blog <br>
+XSS with comments and searchs <br>
---
+++ posts.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+require_once 'database.php';
+if (empty($_GET['postid']) || !is_numeric($postId = $_GET['postid'])) {
+    Header("Location: index.php");
+}
+$query = "SELECT * FROM posts WHERE post_id = $postId";
+$result = mysqli_query($connect, $query);
+$row = mysqli_fetch_assoc($result);
+?>
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/blog.css">
+    <title>Blog Post</title>
+</head>
+
+<body>
+    <!-- navbar start -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/index.php">Home</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2 rounded-0" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success rounded-0 my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+    <!-- navbar ends -->
+    <!-- main content start  -->
+    <div class="container mt-5">
+        <div class="row main-section">
+            <div class="col-sm-12 col-md-9 col-lg-9">
+                <div class="card rounded-0 shadow-sm">
+                    <div class="card-header">
+                        <?php
+
+                        if (mysqli_num_rows($result) != 0) {
+                            echo "
+                                <span>By </span>
+                                <span class='text-success'>{$row['author']}</span>
+                            ";
+                        }
+                        ?>
+                        <?php
+
+                        if (mysqli_num_rows($result) != 0) {
+                            echo "
+                                <span>on </span>
+                                <span class='text-success'>{$row['date']}</span>
+                            ";
+                        } else {
+                            echo "<span style='color:red; font-size: 25px;'>No post found</span>";
+                        }
+                        ?>
+                    </div>
+                    <div class="card-body">
+                        <?php
+                        $file_dir = "img/post" . $postId . ".png";
+                        if (file_exists($file_dir))
+                            echo "<img class='card-img-top' src=$file_dir>";
+                        ?>
+                        <hr>
+                        <?php
+                        echo $row['content'];
+                        ?>
+                    </div>
+                    <div class="card-footer">
+                        <div class="row">
+                            <div class="col col-sm-10 col-md-10">
+                                <div class="form-group">
+                                    <input type="text" name="comment" class="form-control rounded-0" placeholder="Enter comment...">
+                                </div>
+                            </div>
+                            <div class="col col-sm-2 col-md-2">
+                                <button class="btn btn-warning rounded-0">Submit</button>
+                            </div>
+                        </div>
+                        <div class="comment-section">
+                            <?php
+                            $query = "SELECT * FROM comments WHERE post_id = $postId";
+                            $result = mysqli_query($connect, $query);
+
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                echo "<span class='text-success bg-faded'><a style='color:black'>{$row['username']}: </a>{$row['comment_text']}</span>";
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-3 col-lg-3">
+                <div class="card rounded-0 shadow-sm">
+                    <div class="card-header">
+                        Category
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><a href="#">Social</a></li>
+                        <li class="list-group-item"><a href="#">Sports</a></li>
+                        <li class="list-group-item"><a href="#">Technology</a></li>
+                        <li class="list-group-item"><a href="#">Trend</a></li>
+                        <li class="list-group-item"><a href="#">Economics</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Optional JavaScript -->
+
+</body>
+
+</html>
